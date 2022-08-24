f5e494edefca2bb6557c20f6c6674da57b3766a1
yashkoradiya <54938660+yashkoradiya@users.noreply.github.com>
2022-08-24 18:40:52 +0530

    Add files via upload

---
+++ ConsoleApp3/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp3
+{
+    public class Calculator
+    {
+        public int Add( int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class multiply
+{
+    public static int mult(ref int a, int b, int c)
+    {
+        return a * b * c;
+    }
+    public static void Main1()
+    {
+        Console.WriteLine("Enter a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter c:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int product = mult(ref a, b, c);
+        Console.WriteLine($"Product is {product}");
+    }
+}
